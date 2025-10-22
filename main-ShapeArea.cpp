65132121c5b7522bf07d15ab4f0e45cbaa67bc10
arnandadwisaputra <a.dwi.saputra081@students.amikom.ac.id>
2025-10-22 10:14:16 +0700

    Add files via upload

---
+++ main-ShapeArea.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	cout << "Shape Area Calculator" << endl;
+	cout << "1. Triangle" << endl;
+	cout << "2. Rectangle" << endl;
+	cout << "3. Circle" << endl;
+	cout << "Choose a shape (1-3): ";
+	int menuChoice;
+	cin >> menuChoice;
+	
+	system("cls");
+	switch (menuChoice){
+	case 1:{
+		double base, height;
+		cout << "Base: ";
+		cin >> base;
+		cout << "Height: ";
+		cin >> height;
+		double area = 0.5 * base * height;
+		cout << "Area : " << area << endl;
+		break;
+	}
+		
+	case 2: {
+		double length, width;
+		cout << "Length: ";
+		cin >> length;
+		cout << "Width : ";
+		cin >> width;
+		double area = length * width;
+		cout << "Area : " << area << endl;
+		break;
+	}
+		
+	case 3: {
+		const double PI = 3.14159;
+		double radius;
+		cout << "Radius: ";
+		cin >> radius;
+		double area = PI * radius * radius;
+		cout << "Area : " << area << endl;
+		break;
+	}
+		
+	default:
+		cout << "Invalid choice." << endl;
+	}
+	
+}
